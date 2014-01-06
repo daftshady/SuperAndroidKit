025c0e1161e01a670b3b341de66b31d1d96d45f5
daftshady <daftonshady@gmail.com>
2014-01-06 14:38:42 +0900

    comment on sharedpreference

--- src/com/daftshady/superandroidkit/sharedpreferences/SuperSharedPreferences.java
+++ src/com/daftshady/superandroidkit/sharedpreferences/SuperSharedPreferences.java
@@ -18,22 +18,45 @@ public class SuperSharedPreferences {
 		mPreferences = activity.getSharedPreferences(fileName, Context.MODE_PRIVATE);
 	}
 	
+	/**
+	 * Get matched value for key.
+	 * @param key
+	 * @return
+	 * 		`String` value
+	 */
 	public String get(String key) {
 		return mPreferences.getString(key, null);
 	}
 	
+	/**
+	 * Set (key, value) pair to `SharedPreference`
+	 * @param key
+	 * @param value
+	 * @return
+	 */
 	public boolean set(String key, String value) {
 		SharedPreferences.Editor editor = mPreferences.edit();
 		editor.putString(key, value);
 		return editor.commit();
 	}
 	
+	/**
+	 * Remove matched value for `key`
+	 * @param key
+	 * @return
+	 * 		Returns true if removing is success, else false.
+	 */
 	public boolean remove(String key) {
 		SharedPreferences.Editor editor = mPreferences.edit();
 		editor.remove(key);
 		return editor.commit();
 	}
 	
+	/**
+	 * Remove all data from `SharedPreference`
+	 * @return
+	 * 		Returns true if removing is success, else false.
+	 */
 	public boolean flush() {
 		SharedPreferences.Editor editor = mPreferences.edit();
 		editor.clear();
